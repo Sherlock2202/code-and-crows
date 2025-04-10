32629a2bf37ee1becda6eb4235401cf60ba8a074
Sherlock2202 <21VPatel@bishopstopford.com>
2025-04-10 17:01:35 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CodeAndCrowsGame from "./CodeAndCrowsGame";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <CodeAndCrowsGame />
+    </div>
+  );
+};
+
+export default App;
